f070774efd75b8c9fca1db7edfb93f130faa3ad0
finn-nankivell2 <N00221795@iadt.ie>
2024-02-24 00:01:28 +0000

    More data parsing

--- data/parsing.py
+++ data/parsing.py
@@ -2,22 +2,53 @@
 import csv
 import itertools
 from collections import Counter
+from pprint import pprint
 
-TITLE_BASICS = "title.basics.tsv"
+TITLE_BASICS = "input/title.basics.tsv"
+TITLE_RATINGS = "input/title.ratings.tsv"
 
 
-def readswrites(rpath, wpath):
+def reads(rpath, csv_reader=True):
 
 	def outer(f):
 
 		def inner():
-			RFILE = open(rpath)
-			WFILE = open(wpath, "w+")
+			with open(rpath) as rfile:
+				reader = rfile
+				if csv_reader:
+					reader = csv.reader(rfile, delimiter="\t")
+					next(rfile)
 
-			f(RFILE, WFILE)
+				f(reader)
 
-			RFILE.close()
-			WFILE.close()
+		inner.__name__ = f.__name__
+		return inner
+
+	return outer
+
+
+def readswrites(rpath, wpath, csv_reader=True, csv_writer=True):
+
+	def outer(f):
+
+		def inner():
+			rfile = open(rpath)
+			wfile = open(wpath, "w+")
+
+			reader = rfile
+			writer = wfile
+
+			if csv_reader:
+				reader = csv.reader(rfile, delimiter="\t")
+				next(reader)
+
+			if csv_writer:
+				writer = csv.writer(wfile, quotechar="\"", quoting=csv.QUOTE_ALL)
+
+			f(reader, writer)
+
+			rfile.close()
+			wfile.close()
 
 		inner.__name__ = f.__name__
 		return inner
@@ -26,15 +57,12 @@ def readswrites(rpath, wpath):
 
 
 @readswrites(TITLE_BASICS, "outputs/movietypes.csv")
-def extract_movie_variants(rfile, wfile):
+def extract_movie_variants(reader, writer):
 	mtypes = set()
-	reader = csv.reader(rfile, delimiter="\t")
-	next(reader)
 
 	for row in reader:
 		mtypes.add(row[1])
 
-	writer = csv.writer(wfile)
 	writer.writerow(["MTYPE"])
 
 	for item in mtypes:
@@ -42,18 +70,50 @@ def extract_movie_variants(rfile, wfile):
 
 
 @readswrites(TITLE_BASICS, "outputs/genrecounts.csv")
-def count_movie_genres(rfile, wfile):
-	reader = itertools.islice(csv.reader(rfile, delimiter="\t"), 100)
-	next(reader)
-
+def count_movie_genres(reader, writer):
 	counter = Counter(filter(lambda x: x, map(lambda row: row[8] if len(row) >= 9 else None, reader)))
-
 	print(counter)
-	# writer = csv.writer(wfile)
+
+
+@reads(TITLE_RATINGS)
+def compute_average_rating(reader):
+	counter = 0
+	total = 0
+	for row in reader:
+		rating, votes = float(row[1]), int(row[2])
+
+		counter += votes
+		total += rating * votes
+
+	print(f"Average rating: {total / counter}")
+
+
+@readswrites(TITLE_RATINGS, "outputs/ratingdist.csv")
+def extract_movie_rating_distribution(reader, writer):
+	ratings_list = list(zip(range(1, 10), range(2, 11)))
+
+	ratings = {f"{a} - {b}": 0 for a, b in ratings_list}
+
+	for row in reader:
+		rating = float(row[1])
+		for a, b in ratings_list:
+			if a <= rating < b:
+				ratings[f"{a} - {b}"] += 1
+
+	writer.writerow(["RANGE", "COUNT"])
+
+	for row in ratings.items():
+		writer.writerow(row)
+
 
 
 def main():
-	jobs = [count_movie_genres]
+	jobs = [
+		# extract_movie_variants,
+		# coumovgenres,
+		# compute_average_rating,
+		extract_movie_rating_distribution,
+	]
 
 	for job in jobs:
 		print(f"Running job {job.__name__}")
